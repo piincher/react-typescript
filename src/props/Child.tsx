f480b0eed7b8a941f7902e3da91af08ba92ca5e5
piincher <69568495+piincher@users.noreply.github.com>
2022-03-25 11:20:38 +0800

    component with type annotation

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,7 +1,13 @@
+import React from "react"
+
 interface childProps {
     color:string
 }
 
 export const Child = ({color}:childProps) => {
     return <p>{color}</p>
+}
+
+export const ChildAsFc: React.FC<childProps> = ({color}) => {
+    return <div>{color}</div>
 }
